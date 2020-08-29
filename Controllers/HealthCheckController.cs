0d45bacdca921aa0998e2f96124759887e23a61c
Tasloach Wol <TRWol@nationalindemnity.com>
2020-08-28 22:25:26 -0500

    Adding HealthCheckController

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string verbosity = null)
+        {
+            return Ok("Health check passed");
+        }
+    }
+}
